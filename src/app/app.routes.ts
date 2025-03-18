9f9a0fb9a4bd58d7210668642fa44e045aeef2e4
Enciso <dav_vd@hotmail.com>
2025-03-18 14:00:54 -0600

    icono

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,16 @@
 import { Routes } from '@angular/router';
 import { QrScannerComponent } from './qr-scanner/qr-scanner.component';
 
-// export const routes: Routes = [
-//   { path: '', redirectTo: '/scanner', pathMatch: 'full' },
-//   { path: 'scanner', component: QrScannerComponent },
-// ];
-
 export const routes: Routes = [
-  // Redirigir la ruta raíz a /scanner?type=entrada
   {
     path: '',
     redirectTo: 'scanner?type=entrada',
     pathMatch: 'full',
   },
-  // Ruta para el scanner
   {
     path: 'scanner',
     component: QrScannerComponent,
   },
-  // Ruta comodín para manejar rutas no encontradas
   {
     path: '**',
     redirectTo: 'scanner?type=entrada',
